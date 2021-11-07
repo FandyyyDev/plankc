1334244aca933c66fa920bf376f6d5efaccfa130
Fbotzz <nazril.afandi1603@gmail.com>
2021-11-07 12:13:27 +0700

    Run

---
+++ IlhanSykes/Menu.js
@@ -0,0 +1,169 @@
+const menu = (hansykes, SykesWM, hanselamat, ilhansykesSELF, jam, thisDay, day, myMonths, year, pushname) => {
+	return `────「 *${SykesWM}* 」────
+│❏️ *Hai, ${pushname}*
+│❏️ *${hanselamat}*
+│❏️ *Jam : ${jam} WIB*
+│❏️ *${thisDay}, ${day} ${myMonths[bulan]} ${year}*
+
+
+*◪ 𝙇𝙄𝙎𝙏 𝙈𝙀𝙉𝙐*
+│❏️ *${hansykes}tts <id teks>*
+│❏️ *${hansykes}translate <id teks>*
+│❏️ *${hansykes}readmore <teks|teks>*
+│❏️ *${hansykes}sharelock <teks|teks>*
+│❏️ *${hansykes}detikvn <reply audio>*
+│❏️ *${hansykes}detikvid <reply video>*
+
+*◪ 𝙎𝙏𝙄𝘾𝙆𝙀𝙍 𝙈𝙀𝙉𝙐*
+│❏️ *${hansykes}sticker*
+│❏️ *${hansykes}ttp <teks>*
+│❏️ *${hansykes}attp <teks>*
+│❏️ *${hansykes}stikerwa <teks>*
+│❏️ *${hansykes}take <teks|teks>*
+│❏️ *${hansykes}toimg <reply stik>*
+│❏️ *${hansykes}tovideo <reply stik>*
+
+*◪ 𝙎𝙀𝘼𝙍𝘾𝙃 𝙈𝙀𝙉𝙐*
+│❏️ *${hansykes}artinama <teks>*
+│❏️ *${hansykes}artimimpi <teks>*
+│❏️ *${hansykes}wiki <teks>*
+│❏️ *${hansykes}brainly <teks>*
+│❏️ *${hansykes}igstalk <teks>*
+│❏️ *${hansykes}jadwalsholat <kota>*
+│❏️ *${hansykes}lirik <judul>*
+│❏️ *${hansykes}ytsearch <query>*
+│❏️ *${hansykes}pinterest <query>*
+│❏️ *${hansykes}playstore <query>*
+│❏️ *${hansykes}linkwa <query>*
+
+*◪ 𝘿𝙊𝙒𝙉𝙇𝙊𝘼𝘿 𝙈𝙀𝙉𝙐*
+│❏️ *${hansykes}asupan*
+│❏️ *${hansykes}play <judul>*
+│❏️ *${hansykes}play2 <judul>*
+│❏️ *${hansykes}playvid <judul>*
+│❏️ *${hansykes}joox <judul>*
+│❏️ *${hansykes}ytmp3 <link>*
+│❏️ *${hansykes}ytmp4 <link>*
+│❏️ *${hansykes}instagram <link>*
+│❏️ *${hansykes}tiktok <link>*
+
+*◪* 𝙈𝘼𝙆𝙀𝙍 𝙈𝙀𝙉𝙐*
+│❏️️ *${hansykes}nuliskiri <teks>*
+│❏️ *${hansykes}nuliskanan <teks>*
+│❏️ *${hansykes}foliokiri <teks>*
+│❏️ *${hansykes}foliokanan <teks>*
+│❏️ *${hansykes}gura <teks>*
+│❏️ *${hansykes}tokyo <teks>*
+│❏️ *${hansykes}loly <teks>*
+│❏️ *${hansykes}carbon <teks>*
+│❏️ *${hansykes}quote <teks>*
+│❏️ *${hansykes}tahta <teks>*
+│❏️ *${hansykes}neon <teks>*
+│❏️ *${hansykes}glow <teks>*
+│❏️ *${hansykes}coffe <teks>*
+│❏️ *${hansykes}coffe2 <teks>*
+│❏️ *${hansykes}glitch <teks|teks>*
+│❏️ *${hansykes}pubg <teks|teks>*
+│❏️ *${hansykes}battlefield <teks|teks>*
+
+*◪ 𝙁𝙐𝙉 𝙈𝙀𝙉𝙐*
+│❏️ *${hansykes}meme*
+│❏️ *${hansykes}darkjokes*
+│❏️ *${hansykes}truth*
+│❏️ *${hansykes}dare*
+│❏️ *${hansykes}fakta*
+│❏️ *${hansykes}pantun*
+│❏️ *${hansykes}cekganteng*
+│❏️ *${hansykes}cekcantik*
+│❏️ *${hansykes}jadian*
+│❏️ *${hansykes}cantik*
+│❏️ *${hansykes}ganteng*
+│❏️ *${hansykes}jelek*
+│❏️ *${hansykes}beban*
+│❏️ *${hansykes}wibu*
+│❏️ *${hansykes}caklontong*
+│❏️ *${hansykes}tebakgambar*
+│❏️ *${hansykes}family100*
+
+*◪ 𝙉𝙎𝙁𝙒 𝙈𝙀𝙉𝙐*
+│❏️ *${hansykes}waifu*
+│❏️ *${hansykes}loli*
+│❏️ *${hansykes}yuri*
+│❏️ *${hansykes}anal*
+│❏️ *${hansykes}lesbian*
+│❏️ *${hansykes}eroneko*
+│❏️ *${hansykes}bj*
+│❏️ *${hansykes}kitsune*
+│❏️ *${hansykes}pussy*
+│❏️ *${hansykes}baka*
+│❏️ *${hansykes}slap*
+│❏️ *${hansykes}poke*
+│❏️ *${hansykes}neko*
+│❏️ *${hansykes}neko2*
+│❏️ *${hansykes}keta*
+│❏️ *${hansykes}awoo*
+│❏️ *${hansykes}blowjob*
+│❏️ *${hansykes}hentai*
+│❏️ *${hansykes}megumin*
+│❏️ *${hansykes}trapnime*
+
+*◪ 𝘽𝙐𝙂 𝙈𝙀𝙉𝙐*
+│❏️ *${hansykes}allbug*
+│❏️ *${hansykes}troli*
+│❏️ *${hansykes}virgam*
+│❏️ *${hansykes}virvid*
+│❏️ *${hansykes}virgif*
+│❏️ *${hansykes}buginvit*
+│❏️ *${hansykes}buglink*
+│❏️ *${hansykes}bugdoc*
+│❏️ *${hansykes}bugcatalog*
+│❏️ *${hansykes}buglokasi*
+
+*◪ 𝙂𝙍𝙊𝙐𝙋 𝙈𝙀𝙉𝙐*
+│❏️ *${hansykes}welcome*
+│❏️ *${hansykes}antilink*
+│❏️ *${hansykes}antipromosi*
+│❏️ *${hansykes}antivirtex*
+│❏️ *${hansykes}nsfw*
+│❏️ *${hansykes}group*
+│❏️ *${hansykes}sider*
+│❏️ *${hansykes}tagall*
+│❏️ *${hansykes}listadmin*
+│❏️ *${hansykes}rulesgrup*
+│❏️ *${hansykes}linkgrup*
+│❏️ *${hansykes}wame*
+│❏️ *${hansykes}infobot*
+│❏️ *${hansykes}setnamegc <teks>*
+│❏️ *${hansykes}setdeskgc <teks>*
+│❏️ *${hansykes}promote <@tag>*
+│❏️ *${hansykes}demote <@tag>*
+│❏️ *${hansykes}getpict <@tag>*
+│❏️ *${hansykes}kick <@tag>*
+│❏️ *${hansykes}add <number>*
+│❏️ *${hansykes}hidetag <teks>*
+│❏️ *${hansykes}notif <@tag>*
+│❏️ *${hansykes}totag <reply>*
+
+*◪ 𝙊𝙒𝙉𝙀𝙍 𝙈𝙀𝙉𝙐*
+│❏️ *${hansykes}autorespon*
+│❏️ *${hansykes}autorecord*
+│❏️ *${hansykes}wakelock*
+│❏️ *${hansykes}unlock*
+│❏️ *${hansykes}status*
+│❏️ *${hansykes}mode*
+│❏️ *${hansykes}setprefix*
+│❏️ *${hansykes}setname*
+│❏️ *${hansykes}setbio*
+│❏️ *${hansykes}leave*
+│❏️ *${hansykes}bc <teks>*
+│❏️ *${hansykes}jadivirgam <reply>*
+│❏️ *${hansykes}setprofile <reply>*
+│❏️ *${hansykes}setfakeimg <reply>*
+│❏️ *${hansykes}sethumb <reply>*
+│❏️ *${hansykes}upswteks <teks>*
+│❏️ *${hansykes}upswvideo <reply caption>*
+│❏️ *${hansykes}upswimage <reply caption>*
+────「 *${SykesWM}* 」──── `
+}
+
+exports.menu = menu
